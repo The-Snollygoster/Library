8b73153fd30d14945e825c47ebd0857c82dbff6b
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-03-24 21:33:01 +0000

    Add table and display books
    I've added a basic table to the HTML file and
    created the function that iterates through the
    myLibrary array and adds all the information of
    each book into the table.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const myLibrary = [theHobbit, shogun, dune];
+const button = document.getElementById('newBook');
+const table = document.getElementById('table');
 
 function Book(title,author,pages,read) {
     this.title = title;
@@ -7,10 +8,28 @@ function Book(title,author,pages,read) {
     this.read = read;
 };
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '304', 'not');
-const shogun = new Book('Shogun', 'James Clavell', '1136', 'not');
-const dune = new Book('Dune', 'Frank Herbet', '896', 'not');
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '304', 'No');
+const shogun = new Book('Shogun', 'James Clavell', '1136', 'No');
+const dune = new Book('Dune', 'Frank Herbet', '896', 'No');
 
-function addBookToLibrary() {
+const myLibrary = [theHobbit, shogun, dune];
 
-};
+myLibrary.forEach(function (book) {
+    let row = table.insertRow(1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    cell1.innerHTML = book.title;
+    cell2.innerHTML = book.author;
+    cell3.innerHTML = book.pages;
+    cell4.innerHTML = book.read;
+});
+
+// function addBookToLibrary() {
+
+// };
+
+// button.addEventListener('click', () => {
+
+// });
