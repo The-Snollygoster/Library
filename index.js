41d633905ee4f5e4b02e62222b2033455734268d
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-03-25 19:33:43 +0000

    Capture user input
    I've finished the dialog, I've made it capable of
    capturing the user input and displaying it on the
    table. But it will redisplay the entire table, so
    I need a way of clearing the table before calling
    the function again.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const button = document.getElementById('newBook');
 const table = document.getElementById('table');
 const dialog = document.querySelector('dialog');
 const submit = document.querySelector('dialog button');
+const input = document.querySelectorAll('input');
 
 function Book(title,author,pages,read) {
     this.title = title;
@@ -16,32 +17,35 @@ const dune = new Book('Dune', 'Frank Herbet', '896', 'No');
 
 const myLibrary = [theHobbit, shogun, dune];
 
-// I probably need to put this inside another function so that I can call it again after
-// a new book is added.
-myLibrary.forEach(function (book) {
-    let row = table.insertRow(1);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    cell1.innerHTML = book.title;
-    cell2.innerHTML = book.author;
-    cell3.innerHTML = book.pages;
-    cell4.innerHTML = book.read;
-});
-
-// function addBookToLibrary() {
-
-// };
+function displayLibrary() {
+    myLibrary.forEach(function (book) {
+        let row = table.insertRow(1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        cell1.innerHTML = book.title;
+        cell2.innerHTML = book.author;
+        cell3.innerHTML = book.pages;
+        cell4.innerHTML = book.read;
+    });
+};
+displayLibrary();
 
 button.addEventListener('click', () => {
     dialog.showModal();
 });
 
-submit.addEventListener('click', () => {
-    click.preventDefault();
-
-    let newBook = new Book( , , , )
-
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    let title = input[0].value;
+    let author = input[1].value;
+    let pages = input[2].value;
+    let read = input[3].value;
+    let newBook = new Book(`${title}`, `${author}`, `${pages}`, `${read}`);
+    myLibrary.push(newBook);
+    console.log(newBook);
+    console.log(myLibrary);
+    displayLibrary();
     dialog.close();
 });
